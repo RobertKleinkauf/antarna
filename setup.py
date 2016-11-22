6da3bd7e521406cfe08df25646dc2070fab2fb42
Robert Kleinkauf <robkleinkauf@gmail.com>
2016-11-22 12:49:36 +0100

    adapting new names

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="antaRNA",
-    packages=['antaRNA'],
+    name="antarna",
+    packages=['antarna'],
     install_requires=['numpy', 'scipy', 'argparse', 'networkx', 'uuid'],
     version="2.0.1",
     description="Ant colony optimized RNA secondary structure inverse folding",
     author='Robert Kleinkauf',
     author_email='robkleinkauf@gmail.com',
-    url='https://github.com/RobertKleinkauf/antaRNA',
+    url='https://github.com/RobertKleinkauf/antarna',
     keywords=['ant colony optimization', 'RNA inverse folding'],
     classifiers=[
         'Intended Audience :: Developers',
