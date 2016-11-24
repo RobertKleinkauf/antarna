ab02321ac652055d91d19350e2db89d395bec2ee
Björn Grüning <bjoern@gruenings.eu>
2016-11-24 08:18:48 +0100

    remove argparse from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="antarna",
     packages=['antarna'],
-    install_requires=['numpy', 'scipy', 'argparse', 'networkx', 'uuid'],
+    install_requires=['numpy', 'scipy', 'networkx', 'uuid'],
     version="2.0.1.1",
     scripts=['antarna/antarna.py'],
     description="Ant colony optimized RNA secondary structure inverse folding",
